372e0ba8bbf3c87e6469d551bb505eec7d742f76
Irtiza Ali <muhammadahad211126@gmail.com>
2025-11-15 11:48:10 +0500

    rename root file

--- api/index.js
+++ api/index.js
@@ -38,6 +38,6 @@ app.use("/api/doctor",require("./routes/doctorRoute"));
 app.use("/api/admin",require("./routes/adminRoute"));
 
 
-// app.listen(PORT,()=>{
-//     console.log(`Server is running on http://localhost:${PORT}`);
-// })
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
